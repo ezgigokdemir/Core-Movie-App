efcc3721b3abbeb9b2399afc0eb040f0bda54d42
Ezgi Gökdemir <38529111+ezgigokdemir@users.noreply.github.com>
2019-05-28 18:14:23 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMovieAppRepeat.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMovieAppRepeat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int? Id)
+        {
+            var movies = MovieRepository.Movies;
+            if (Id != null)
+            {
+                movies = movies.Where(x => x.CategoryID == Id).ToList();
+            }
+            return View(movies);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            var movie = MovieRepository.GetByID(Id);
+            return View(movie);
+        }
+    }
+}
